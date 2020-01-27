1ab45a4e3f4c33ce9e76ec56551bc652744bf84b
sudipto dutta <sudiptodut@gmail.com>
2020-01-27 20:15:38 +0530

    CheckInstall

---
+++ CheckInstall/src/test/java/com/test/qa/checkInstallTest.java
@@ -0,0 +1,26 @@
+package com.test.qa;
+
+import org.testng.annotations.Test;
+
+public class checkInstallTest {
+
+	@Test
+	public void sayHello() {
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void saySum() {
+		System.out.println("Add Here");
+	}
+	
+	@Test
+	public void sayPrint() {
+		System.out.println("Print Here");
+	}
+	
+	@Test
+	public void sayBye() {
+		System.out.println("Bye");
+	}
+}
